74c1ea0f5c7fa13185b14900beb74d61ec6237a2
Abdul Muizz <azikumapayi@gmail.com>
2023-08-15 12:40:37 +0000

    add a template_folder to display items

--- devices/phone/models.py
+++ devices/phone/models.py
@@ -6,6 +6,7 @@ class Item(models.Model):
     item_name = models.CharField(db_column="Item_Name", max_length=200)
     item_desc = models.CharField(db_column="Item_Desc", max_length=200)
     item_price = models.IntegerField(db_column="Item_Price",)
+    item_image = models.CharField(db_column="item_image", max_length=5000, default="https://www.shutterstock.com/image-photo/phone-placeholder-that-can-be-260nw-1604743003.jpg")
 
     def __str__(self) -> str:
         return f"{self.item_name}"
